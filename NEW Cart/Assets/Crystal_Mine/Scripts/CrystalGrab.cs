1a43c622e807e3cfcdcd1635c94b8dea617402cd
VeronikaIsHere <veronika.frei2002@gmail.com>
2023-06-28 21:06:03 +0200

    game fix
    script geändert auf controller getan und collider auf floor und mincart leicht nach oben verschoben

--- NEW Cart/Assets/Crystal_Mine/Scripts/CrystalGrab.cs
+++ NEW Cart/Assets/Crystal_Mine/Scripts/CrystalGrab.cs
@@ -4,8 +4,8 @@ public class CrystalGrab : MonoBehaviour
 {
     public string crystalTag = "Crystal";
     public Score scoreScript;
-    public Collider leftHandCollider;
-    public Collider rightHandCollider;
+    // public Collider leftHandCollider;
+    // public Collider rightHandCollider;
     public MinecartMovement minecartMovement;
 
     private AudioSource _audioSource;
@@ -16,48 +16,48 @@ public class CrystalGrab : MonoBehaviour
     }
     private void Update()
     {
-        CheckCollision(leftHandCollider);
-        CheckCollision(rightHandCollider);
+        // CheckCollision(leftHandCollider);
+        // CheckCollision(rightHandCollider);
     }
 
-    private void CheckCollision(Collider collider)
-    {
-        // Check if the collider is valid and active
-        if (collider != null && collider.enabled && collider.gameObject.activeInHierarchy)
-        {
-            // Get all colliders overlapping with the collider's bounding box
-            Collider[] colliders = Physics.OverlapBox(collider.bounds.center, collider.bounds.extents, collider.transform.rotation);
+    // private void CheckCollision(Collider collider)
+    // {
+    //     // Check if the collider is valid and active
+    //     if (collider != null && collider.enabled && collider.gameObject.activeInHierarchy)
+    //     {
+    //         // Get all colliders overlapping with the collider's bounding box
+    //         Collider[] colliders = Physics.OverlapBox(collider.bounds.center, collider.bounds.extents, collider.transform.rotation);
 
-            // Iterate through each collider
-            foreach (Collider otherCollider in colliders)
-            {
-                // Check if the collider has the crystal tag
-                // TODO: Copy over as soon as hand colliders are implemented
-                if (otherCollider.CompareTag(crystalTag))
-                {
-                    _audioSource.time = 0.5f;
-                    _audioSource.Play();
-                    CollectCrystal(otherCollider.gameObject);
-                    scoreScript.IncrementScore(1);
-                    minecartMovement.IncrementCrystalsCollected();
-                }
-            }
+    //         // Iterate through each collider
+    //         foreach (Collider otherCollider in colliders)
+    //         {
+
+    //             // Check if the collider has the crystal tag
+    //             // TODO: Copy over as soon as hand colliders are implemented
+    //             if (otherCollider.CompareTag(crystalTag))
+    //             {
+    //                 
+    //             }
+    //         }
+    //     }
+    // }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(crystalTag))
+        {
+            Debug.Log("Collect crystal now!");
+            _audioSource.time = 0.5f;
+            _audioSource.Play();
+            CollectCrystal(other.gameObject);
+            scoreScript.IncrementScore(1);
+            minecartMovement.IncrementCrystalsCollected();
         }
     }
 
-    // private void OnTriggerEnter(Collider other)
-    // {
-    //     if (other.CompareTag(crystalTag))
-    //             {
-    //                 CollectCrystal(other.gameObject);
-    //                 scoreScript.IncrementScore(1);
-    //                 minecartMovement.IncrementCrystalsCollected();
-    //             }
-    // }
-
     private void CollectCrystal(GameObject crystal)
     {
-        
+
         // Deactivate the crystal
         crystal.SetActive(false);
     }
